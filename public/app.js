efb080a37fdcc37099e16fe778de6d1ec6f99eff
kostya zalilov <zalilov.k@gmail.com>
2022-01-24 02:25:01 +0500

    task2 fix 1

--- public/app.js
+++ public/app.js
@@ -1,27 +1,14 @@
 document.addEventListener('click', event => {
   const id = event.target.dataset.id
   if (event.target.dataset.type === 'remove') {
-  remove(id).then(() => {
-  event.target.closest('li').remove()
-  })
-  } else if (event.target.dataset.type === 'edit') {
-  const title = event.target.dataset.title
-  const result = prompt(title, title)
-  const currentLi = event.target.closest('li')
-  const delBtn = event.target.previousElementSibling
-  const editBtn = event.target
-  edit(id, result).then(() => {
-  event.target.closest('li').innerHTML = ''
-  currentLi.innerText = result ? result : title
-  editBtn.dataset.title = result ? result : title
-  currentLi.appendChild(delBtn)
-  currentLi.appendChild(editBtn)
+    remove(id).then(() => {
+    event.target.closest('li').remove()
   })
   } else if (event.target.dataset.type === 'update') {
     console.log(event.target.closest('button'))
     toUpdate(event, event.target.closest('button'), id)
   } 
-  })
+})
 
   function toUpdate(event, btnUpdate, id) {
     const currentLi = event.target.closest('li')
