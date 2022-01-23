7bbeabab4eb9d8322d687908545747816ccd5081
kostya zalilov <zalilov.k@gmail.com>
2022-01-23 17:36:37 +0500

    changed in app

--- public/app.js
+++ public/app.js
@@ -6,13 +6,14 @@ document.addEventListener('click', event => {
     })
   } else if (event.target.dataset.type === 'edit') {
     const title = event.target.dataset.title
+    const result = prompt(title, title)
     const currentLi = event.target.closest('li')
     const delBtn = event.target.previousElementSibling
     const editBtn = event.target
-    edit(id, title).then((value) => {
+    edit(id, result).then(() => {
       event.target.closest('li').innerHTML = ''
-      currentLi.innerText = value
-      editBtn.dataset.title = value
+      currentLi.innerText = result ? result : title
+      editBtn.dataset.title = result ? result : title
       currentLi.appendChild(delBtn)
       currentLi.appendChild(editBtn)
     })
@@ -23,8 +24,7 @@ async function remove(id) {
   await fetch(`/${id}`, { method: 'DELETE' })
 }
 
-async function edit(id, title) {
-  const result = prompt(title, title)
+async function edit(id, result) {
   const params = {
     param: result
   }
@@ -36,6 +36,5 @@ async function edit(id, title) {
     body: JSON.stringify( params )  
   }
   await fetch(`/${id}`, options)
-  if (result === null) return title
-  return result
+
 }
